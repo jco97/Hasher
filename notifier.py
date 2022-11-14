07359a2b9caa94cac93eaa47dc6120a33bf9a659
Javier <112417046+jco97@users.noreply.github.com>
2022-11-14 13:50:46 -0800

    Add files via upload

---
+++ notifier.py
@@ -0,0 +1,13 @@
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+def send_notification(url):
+    post_fields = {
+        'k':'xxxxxxxxxxxxxxxxxxxx',
+        't':'New PDF found!',
+        'd':'a',
+        'm':'A new PDF was found.',
+        'u':url
+    }
+    ps = 'https://www.pushsafer.com/api'
+    request = Request(ps, urlencode(post_fields).encode())
+    json = urlopen(request).read().decode()
